fdadc9bc2c3094a1ce81426d0fe431d92dbf62db
Isfiaya <aymen.isfiaya@gmail.com>
2025-09-28 16:40:48 +0100

    feat: Add Pokemon types and interfaces for API responses

---
+++ src/types/pokemon.ts
@@ -0,0 +1,144 @@
+// Pokemon List API Response
+export interface PokemonListResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: PokemonListItem[];
+}
+
+export interface PokemonListItem {
+  name: string;
+  url: string;
+}
+
+// Pokemon Detail API Response
+export interface PokemonDetail {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  base_experience: number;
+  order: number;
+  is_default: boolean;
+  sprites: PokemonSprites;
+  abilities: PokemonAbility[];
+  forms: PokemonForm[];
+  game_indices: GameIndex[];
+  held_items: HeldItem[];
+  location_area_encounters: string;
+  moves: PokemonMove[];
+  species: PokemonSpecies;
+  stats: PokemonStat[];
+  types: PokemonType[];
+}
+
+export interface PokemonSprites {
+  back_default: string | null;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+  front_default: string | null;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+  other?: {
+    dream_world?: {
+      front_default: string | null;
+      front_female: string | null;
+    };
+    home?: {
+      front_default: string | null;
+      front_female: string | null;
+      front_shiny: string | null;
+      front_shiny_female: string | null;
+    };
+    'official-artwork'?: {
+      front_default: string | null;
+      front_shiny: string | null;
+    };
+  };
+}
+
+export interface PokemonAbility {
+  is_hidden: boolean;
+  slot: number;
+  ability: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonForm {
+  name: string;
+  url: string;
+}
+
+export interface GameIndex {
+  game_index: number;
+  version: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface HeldItem {
+  item: {
+    name: string;
+    url: string;
+  };
+  version_details: VersionDetail[];
+}
+
+export interface VersionDetail {
+  rarity: number;
+  version: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonMove {
+  move: {
+    name: string;
+    url: string;
+  };
+  version_group_details: VersionGroupDetail[];
+}
+
+export interface VersionGroupDetail {
+  level_learned_at: number;
+  move_learn_method: {
+    name: string;
+    url: string;
+  };
+  version_group: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonSpecies {
+  name: string;
+  url: string;
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonListDisplayItem extends PokemonListItem {
+  id: number;
+}
